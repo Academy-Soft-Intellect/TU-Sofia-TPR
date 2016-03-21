257c83ab8c0b9be96813f8a1a6987332c1559c08
sestrimski <sestrimski@gmail.com>
2016-03-21 02:34:45 +0200

    New Version

---
+++ Excercise 5/CurrencyConverter/CurrencyConverter/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace CurrencyConverter
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrancyComboBox.SelectionBoxItem == null)
+            {
+                ConvertedTextBox.Text = ValueTextBox.Text;
+                return;
+            }
+            switch (CurrancyComboBox.SelectionBoxItem.ToString())
+            {
+                case "EUR (Euro)":
+                    ConvertedTextBox.Text = (double.Parse(ValueTextBox.Text) / 1.9569).ToString(CultureInfo.InvariantCulture);
+                    break;
+
+                case "USD (US Dollar)":
+                    ConvertedTextBox.Text = (double.Parse(ValueTextBox.Text) / 1.7636).ToString(CultureInfo.InvariantCulture);
+                    break;
+
+                case "GBP (British pound)":
+                    ConvertedTextBox.Text = (double.Parse(ValueTextBox.Text) / 2.52056).ToString(CultureInfo.InvariantCulture);
+                    break;
+
+                default:
+                    ConvertedTextBox.Text = ValueTextBox.Text;
+                    break;
+            }
+        }
+    }
+}
---
+++ Excersice 2/Test123/Test123/src/Test123123.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Test123123 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Test123123 window = new Test123123();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Test123123() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
